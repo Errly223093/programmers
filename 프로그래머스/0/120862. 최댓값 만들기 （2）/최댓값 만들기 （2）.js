adbc25333f29166dccffc005c04169694b5b06c1
HyeongSeok <127872822+Errly223093@users.noreply.github.com>
2024-01-24 21:36:12 +0800

    [level 0] Title: 최댓값 만들기 (2), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,22 +1,4 @@
 function solution(numbers) {
-//     let arr = [];
-//     let max;
-//     let max2;
-    
-//     for(let i = 0; i < numbers.length; i++){
-//         arr[i] = Math.abs(numbers[i]);
-//     }
-    
-//     for(let i = 0; i < numbers.length; i++){
-//         max = Math.max(...arr);
-//         arr[arr.indexOf(max)] = 0;
-//         max2 = Math.max(...arr);
-//         if(
-            
-//         ){
-//             return max * max2;
-//         }
-//     }
     let arr = [];
     
     for(let i = 0; i < numbers.length-1; i++){
