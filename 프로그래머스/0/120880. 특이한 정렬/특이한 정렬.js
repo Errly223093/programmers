4d7db890a616ddc48f6b37e1772cfa7fc7390405
HyeongSeok <127872822+Errly223093@users.noreply.github.com>
2024-02-11 19:56:24 +0800

    [level 0] Title: 특이한 정렬, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,18 @@
+function solution(numlist, n) {
+    let answer = [];
+    let arr = [];
+    
+    for(let i = 0; i < numlist.length-1; i++){
+        for(let j = i+1; j < numlist.length; j++){
+            if(Math.abs(numlist[i] - n) === Math.abs(numlist[j] - n)){
+                if(numlist[i] < numlist[j]) {
+                    [numlist[i], numlist[j]] = [numlist[j], numlist[i]];
+                }
+            } else if(Math.abs(numlist[i] - n) > Math.abs(numlist[j] - n)){
+                [numlist[i], numlist[j]] = [numlist[j], numlist[i]];
+            }
+        }
+    }
+    
+    return numlist
+}
