f91ffd8d92b2b41205c0230fae092b6aeb16aa52
HyeongSeok <127872822+Errly223093@users.noreply.github.com>
2024-02-16 21:56:22 +0800

    [level 0] Title: 옹알이 (1), Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,19 @@
+function solution(say) {
+    let word = ["aya", "ye", "woo", "ma"];
+    let index = new Array(say.length).fill(0);
+    
+    for(let i = 0; i < say.length; i++){
+        let a = say[i];
+        let count = a.length;
+
+        for(let j = 0; j < word.length; j++){
+            if(a.indexOf(word[j]) >= 0){
+                index[i] += 1;
+                count -= word[j].length;
+            }
+        }
+        (count === 0) ? null : index[i] = 0;
+    }
+    
+    return index.filter((x) => x > 0).length;
+}
